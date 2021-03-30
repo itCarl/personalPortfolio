f4ffe55109afa772caa4a3c7d8922ef07957223e
itCarl <mewes.maximilian@gmx.de>
2021-03-31 01:09:09 +0200

    ionos fix

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::resource('projects', 'ProjectDetailController');
 // but i only want a hidden login available so i do the auth routes manually.
 // disabeling certain routes gets available with laravel 5.7 and higher
 
-//$login_uri = '4ebd0208-8328-5d69-8c44-ec50939c0967';
-$login_uri = 'login';
+$login_uri = '4ebd0208-8328-5d69-8c44-ec50939c0967';
+//$login_uri = 'login';
 
 // Authentication Routes...
 $this->get($login_uri, 'Auth\LoginController@showLoginForm')->name('login');
@@ -32,8 +32,8 @@ $this->post($login_uri, 'Auth\LoginController@login');
 $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
-$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-$this->post('register', 'Auth\RegisterController@register');
+//$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+//$this->post('register', 'Auth\RegisterController@register');
 
 // Password Reset Routes...
 //$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
